e2d0ed67a42efcfb715aad73d6feae19ec9ee5d6
eliyash <eliyash3@gmail.com>
2025-11-27 20:53:37 +0200

    read args from clearml

--- agent_training_code/train_chimp_face_id.py
+++ agent_training_code/train_chimp_face_id.py
@@ -751,4 +751,19 @@ def main():
 
 
 if __name__ == "__main__":
+    from clearml import Task
+
+    task = Task.init(
+        # project_name='training_identity_bboxes_in_video_frames',  # project name of at least 3 characters
+        # task_name='Training',  # task name of at least 3 characters
+        task_type=None,
+        tags=None,
+        reuse_last_task_id=False,
+        continue_last_task=False,
+        output_uri=None,
+        auto_connect_arg_parser=True,
+        # auto_connect_frameworks=True,
+        # auto_resource_monitoring=True,
+        # auto_connect_streams=True,
+    )
     main()
